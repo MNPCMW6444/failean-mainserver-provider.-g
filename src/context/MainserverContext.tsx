10a1538bfcddb5182c61750838b4c3a7e326499a
Michael Nusair‬‏ <mnpcmw6444@gmail.com>
2023-06-26 11:08:53 +0300

    asd

--- src/context/MainserverContext.tsx
+++ src/context/MainserverContext.tsx
@@ -7,7 +7,15 @@ import React, {
 } from "react";
 import { Typography } from "@mui/material";
 import axios, { AxiosInstance } from "axios";
-import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  HttpLink,
+  split,
+} from "@apollo/client";
+import { getMainDefinition } from "@apollo/client/utilities";
+import { WebSocketLink } from "@apollo/client/link/ws";
 
 interface MainserverProviderProps {
   children: ReactNode;
@@ -64,8 +72,34 @@ export const MainserverProvider = ({
     },
   });
 
-  const client = new ApolloClient({
+  const httpLink = new HttpLink({
     uri: baseURL + "graphql",
+  });
+
+  const wsLink = new WebSocketLink({
+    uri:
+      process.env.NODE_ENV === "development"
+        ? "ws://localhost:6555/graphql"
+        : `wss://${env || ""}mainserver.failean.com/graphql`,
+    options: {
+      reconnect: true,
+    },
+  });
+
+  const link = split(
+    ({ query }) => {
+      const definition = getMainDefinition(query);
+      return (
+        definition.kind === "OperationDefinition" &&
+        definition.operation === "subscription"
+      );
+    },
+    wsLink,
+    httpLink
+  );
+
+  const client = new ApolloClient({
+    link,
     cache: new InMemoryCache(),
   });
 
