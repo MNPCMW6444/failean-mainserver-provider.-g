c1550a2df31dddf748086d1ca16c6aec7ad8b711
ovaidov <106985580+YoadNatan@users.noreply.github.com>
2023-07-11 17:02:58 +0300

    Update MainserverContext.tsx

--- src/context/MainserverContext.tsx
+++ src/context/MainserverContext.tsx
@@ -52,6 +52,7 @@ interface MainserverProviderProps {
   children: ReactNode;
   tryInterval?: number;
   env?: "tst" | "dev";
+  onError?:ReactNode;
 }
 
 const DEFAULT_TRY_INTERVAL = 3000;
@@ -69,6 +70,7 @@ export const MainserverProvider = ({
   children,
   tryInterval,
   env,
+  onError
 }: MainserverProviderProps) => {
   const interval = tryInterval || DEFAULT_TRY_INTERVAL;
   const IDLE = "IDLE";
@@ -176,6 +178,6 @@ export const MainserverProvider = ({
       </MainserverContext.Provider>
     );
   } else {
-    return <Typography>{status}</Typography>;
+    return onError || <Typography>{status}</Typography>;
   }
 };
